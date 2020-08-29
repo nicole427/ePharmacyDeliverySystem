bf87a4c24434e944a66f1c213c270e443eb02880
Ayabulela Mahlathini <atrad75@gmail.com>
2020-08-29 23:10:54 +0200

    Order Repository & Test Created

---
+++ src/main/java/com/digital/epharmacy/repository/Order/OrderRepository.java
@@ -0,0 +1,15 @@
+package com.digital.epharmacy.repository.Order;
+
+import com.digital.epharmacy.entity.Order.Order;
+import com.digital.epharmacy.repository.Repository;
+
+import java.util.Set;
+
+/** Author: Ayabulela Mahlathini - 218017774
+ * Date: 29/08/2020
+ * Description: Interface for getting all orders from the database
+ */
+
+public interface OrderRepository extends Repository<Order, String> {
+    Set<Order> getAll();
+}
