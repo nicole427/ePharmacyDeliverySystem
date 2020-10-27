256fa2179a0f8873fd0f74d3042d7be082a79cde
Chad Boswell <chadboswell19@gmail.com>
2020-10-27 19:44:34 +0200

    DriverCar, DriverProfile- services, repository, entities completed

--- src/test/java/com/digital/epharmacy/repository/Driver/impl/DriverCarRepositoryImplTest.java
+++ src/test/java/com/digital/epharmacy/repository/Driver/impl/DriverCarRepositoryImplTest.java
@@ -1,68 +1,69 @@
-package com.digital.epharmacy.repository.Driver.impl;
+//package com.digital.epharmacy.repository.Driver.impl;
 /*
-    Author: Chad Boswell
-    Desc: DriverCar repository test case
-    Date: 30th August 2020
- */
-import com.digital.epharmacy.entity.Driver.DriverCar;
-import com.digital.epharmacy.entity.User.UserProfile;
-import com.digital.epharmacy.factory.Driver.DriverCarFactory;
-import com.digital.epharmacy.factory.Pharmacy.PharmacyFactory;
-import com.digital.epharmacy.factory.User.UserProfileFactory;
-import com.digital.epharmacy.repository.Driver.impl.DriverCarRepositoryImpl;
-import com.digital.epharmacy.repository.Driver.DriverCarRepository;
-import org.junit.Assert;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.runners.MethodSorters;
-
-import static org.junit.Assert.*;
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class DriverCarRepositoryImplTest {
-
-    private static DriverCarRepository repository = DriverCarRepositoryImpl.getRepository();
-    private static DriverCar driverCar = DriverCarFactory.createDriverCar("Test","CY 240", "blue", "Ford", "Figo");
-
-    @Order(1)
-    @Test
-    public void a_create() {
-        DriverCar created = repository.create(driverCar);
-        Assert.assertEquals(driverCar.getDriverId(), created.getDriverId());
-        System.out.println("Created: " + created);
-    }
-
-    @Order(2)
-    @Test
-    public void b_read() {
-        DriverCar read = repository.read(driverCar.getDriverId());
-        System.out.println("Read: " + read);
-    }
-
-    @Order(3)
-    @Test
-    public void c_update() {
-        DriverCar updated = new DriverCar.Builder().copy(driverCar).setCarName("Toyota").builder();
-        updated = repository.update(updated);
-        System.out.println("Updated: " + updated);
-    }
-
-    @Order(5)
-    @Test
-    public void e_delete() {
-        boolean deleted = repository.delete(driverCar.getDriverId());
-        Assert.assertTrue(deleted);
-
-        if (deleted)
-            System.out.println("Entry Deleted");
-    }
-
-    @Order(4)
-    @Test
-    public void d_getAll() {
-        System.out.println("Get All: " + repository.getAll());
-    }
-}
+//    Author: Chad Boswell
+//    Desc: DriverCar repository test case
+//    Date: 30th August 2020
+// */
+//import com.digital.epharmacy.entity.Driver.DriverCar;
+//import com.digital.epharmacy.entity.User.UserProfile;
+//import com.digital.epharmacy.factory.Driver.DriverCarFactory;
+//import com.digital.epharmacy.factory.Pharmacy.PharmacyFactory;
+//import com.digital.epharmacy.factory.User.UserProfileFactory;
+//import com.digital.epharmacy.repository.Driver.impl.DriverCarRepositoryImpl;
+//import com.digital.epharmacy.repository.Driver.DriverCarRepository;
+//import org.junit.Assert;
+//import org.junit.FixMethodOrder;
+//import org.junit.Test;
+//import org.junit.jupiter.api.MethodOrderer;
+//import org.junit.jupiter.api.Order;
+//import org.junit.jupiter.api.TestMethodOrder;
+//import org.junit.runners.MethodSorters;
+//
+//import static org.junit.Assert.*;
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+//public class DriverCarRepositoryImplTest {
+//
+//    private static DriverCarRepository repository = DriverCarRepositoryImpl.getRepository();
+//    private static DriverCar driverCar = DriverCarFactory.createDriverCar("Test","CY 240", "blue", "Ford", "Figo");
+//
+//    @Order(1)
+//    @Test
+//    public void a_create() {
+//        DriverCar created = repository.create(driverCar);
+//        Assert.assertEquals(driverCar.getDriverId(), created.getDriverId());
+//        System.out.println("Created: " + created);
+//    }
+//
+//    @Order(2)
+//    @Test
+//    public void b_read() {
+//        DriverCar read = repository.read(driverCar.getDriverId());
+//        System.out.println("Read: " + read);
+//    }
+//
+//    @Order(3)
+//    @Test
+//    public void c_update() {
+//        DriverCar updated = new DriverCar.Builder().copy(driverCar).setCarName("Toyota").builder();
+//        updated = repository.update(updated);
+//        System.out.println("Updated: " + updated);
+//    }
+//
+//    @Order(5)
+//    @Test
+//    public void e_delete() {
+//        boolean deleted = repository.delete(driverCar.getDriverId());
+//        Assert.assertTrue(deleted);
+//
+//        if (deleted)
+//            System.out.println("Entry Deleted");
+//    }
+//
+//    @Order(4)
+//    @Test
+//    public void d_getAll() {
+//        System.out.println("Get All: " + repository.getAll());
+//    }
+//}
+//
--- src/test/java/com/digital/epharmacy/repository/Driver/impl/DriverProfileRepositoryImplTest.java
+++ src/test/java/com/digital/epharmacy/repository/Driver/impl/DriverProfileRepositoryImplTest.java
@@ -1,76 +1,76 @@
-package com.digital.epharmacy.repository.Driver.impl;
-/*
- * Author: Chad Boswell
- * Date: 30/08/2020
- * Desc: DriverProfileRepository Test
- */
-
-
-import com.digital.epharmacy.entity.Driver.DriverProfile;
-import com.digital.epharmacy.factory.Driver.DriverProfileFactory;
-import com.digital.epharmacy.repository.Driver.DriverProfileRepository;
-import com.digital.epharmacy.repository.Driver.impl.DriverProfileRepositoryImpl;
-import org.junit.Assert;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.runners.MethodSorters;
-
-import java.util.Set;
-
-import static org.junit.Assert.*;
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-
-public class DriverProfileRepositoryImplTest {
-    private static DriverProfileRepository repository = new DriverProfileRepositoryImpl();
-    private static DriverProfile  driverProfile = DriverProfileFactory.createDriverProfile(
-            "Chaddo",
-            "Bossi",
-            "Male");
-    @Order(1)
-    @Test
-    public void a_create() {
-        DriverProfile created = repository.create(driverProfile);
-        assertEquals(driverProfile.getDriverId(), created.getDriverId());
-        System.out.println("Create: " + created);
-    }
-
-    @Order(2)
-    @Test
-    public void b_read() {
-        DriverProfile read = repository.read(driverProfile.getDriverId());
-        assertEquals(driverProfile.getDriverId(),read.getDriverId());
-        System.out.println("Read: " + read);
-    }
-
-    @Order(3)
-    @Test
-    public void c_update() {
-        DriverProfile updated = new DriverProfile.Builder().copy(driverProfile).setDriverName("Barry").setDriverSurname("Bossi").builder();
-        updated = repository.update(updated);
-        assertEquals(driverProfile.getDriverId(), updated.getDriverId());
-        assertNotEquals(driverProfile.getDriverName(), updated.getDriverName());
-        System.out.println("Update: " + updated );
-    }
-
-    @Order(5)
-    @Test
-    public void e_delete() {
-        boolean deleted =   repository.delete(driverProfile.getDriverId());
-        Assert.assertTrue(deleted);
-        if (deleted){
-            System.out.println("Entry deleted");
-        }
-    }
-
-    @Order(4)
-    @Test
-    public void d_getAll() {
-        Set<DriverProfile> driverProfile = repository.getAll();
-        assertEquals(1, driverProfile.size());
-        System.out.println(repository.getAll());
-    }
-}
+//package com.digital.epharmacy.repository.Driver.impl;
+///*
+// * Author: Chad Boswell
+// * Date: 30/08/2020
+// * Desc: DriverProfileRepository Test
+// */
+//
+//
+//import com.digital.epharmacy.entity.Driver.DriverProfile;
+//import com.digital.epharmacy.factory.Driver.DriverProfileFactory;
+//import com.digital.epharmacy.repository.Driver.DriverProfileRepository;
+//import com.digital.epharmacy.repository.Driver.impl.DriverProfileRepositoryImpl;
+//import org.junit.Assert;
+//import org.junit.FixMethodOrder;
+//import org.junit.Test;
+//import org.junit.jupiter.api.MethodOrderer;
+//import org.junit.jupiter.api.Order;
+//import org.junit.jupiter.api.TestMethodOrder;
+//import org.junit.runners.MethodSorters;
+//
+//import java.util.Set;
+//
+//import static org.junit.Assert.*;
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+//
+//public class DriverProfileRepositoryImplTest {
+//    private static DriverProfileRepository repository = new DriverProfileRepositoryImpl();
+//    private static DriverProfile  driverProfile = DriverProfileFactory.createDriverProfile(
+//            "Chaddo",
+//            "Bossi",
+//            "Male");
+//    @Order(1)
+//    @Test
+//    public void a_create() {
+//        DriverProfile created = repository.create(driverProfile);
+//        assertEquals(driverProfile.getDriverId(), created.getDriverId());
+//        System.out.println("Create: " + created);
+//    }
+//
+//    @Order(2)
+//    @Test
+//    public void b_read() {
+//        DriverProfile read = repository.read(driverProfile.getDriverId());
+//        assertEquals(driverProfile.getDriverId(),read.getDriverId());
+//        System.out.println("Read: " + read);
+//    }
+//
+//    @Order(3)
+//    @Test
+//    public void c_update() {
+//        DriverProfile updated = new DriverProfile.Builder().copy(driverProfile).setDriverName("Barry").setDriverSurname("Bossi").builder();
+//        updated = repository.update(updated);
+//        assertEquals(driverProfile.getDriverId(), updated.getDriverId());
+//        assertNotEquals(driverProfile.getDriverName(), updated.getDriverName());
+//        System.out.println("Update: " + updated );
+//    }
+//
+//    @Order(5)
+//    @Test
+//    public void e_delete() {
+//        boolean deleted =   repository.delete(driverProfile.getDriverId());
+//        Assert.assertTrue(deleted);
+//        if (deleted){
+//            System.out.println("Entry deleted");
+//        }
+//    }
+//
+//    @Order(4)
+//    @Test
+//    public void d_getAll() {
+//        Set<DriverProfile> driverProfile = repository.getAll();
+//        assertEquals(1, driverProfile.size());
+//        System.out.println(repository.getAll());
+//    }
+//}
