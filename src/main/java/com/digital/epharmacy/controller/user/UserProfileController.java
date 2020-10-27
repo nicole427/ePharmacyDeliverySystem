fc7846997663abbaf97dd246289eca83fd76b011
nicole427 <nicolehawthorne47@gmail.com>
2020-10-27 21:16:50 +0200

    Cleaned Pom file and added dependencies for jpa and mysql. Update UserProfile entity to map the entity to a table in epharmacy db Made changes to Repository, services and controller implementation classes for UserProfile. Added readByUserProfileName method. Tested Services and Controllers for UserProfile.

--- src/main/java/com/digital/epharmacy/controller/user/UserProfileController.java
+++ src/main/java/com/digital/epharmacy/controller/user/UserProfileController.java
@@ -1,13 +1,17 @@
 package com.digital.epharmacy.controller.user;
 //Nicole Hawthorne - UserProfile Controller to expose the service of the userprofile
+//on 27/10/2020 Nicole made changes to controller to include read method by userName and added validation service
 import com.digital.epharmacy.entity.User.UserProfile;
 import com.digital.epharmacy.factory.User.UserProfileFactory;
-import com.digital.epharmacy.service.User.UserProfileService;
 import com.digital.epharmacy.service.User.impl.UserProfileServiceImpl;
-import org.apache.catalina.User;
+import com.digital.epharmacy.service.Validation.ValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Set;
 
 @RestController
@@ -16,17 +20,37 @@ public class UserProfileController {
     @Autowired
     private UserProfileServiceImpl userProfileService;
 
-@PostMapping ("/create")
-public UserProfile create (@RequestBody UserProfile userProfile){
-    UserProfile newUserProfile = UserProfileFactory.createUserProfile(userProfile.getUserName(),userProfile.getUserSurname(),userProfile.getGender());
-      return userProfileService.create(newUserProfile);
+    @Autowired
+    private ValidationService validationService;
+
+    @PostMapping("/create/")
+    public ResponseEntity<UserProfile> create(@Valid @RequestBody UserProfile userProfile, BindingResult bindingResult) {
+        ResponseEntity<UserProfile> errorMap = (ResponseEntity<UserProfile>) validationService.MapValidationService(bindingResult);
+        if (errorMap != null) {
+            return errorMap;
+        }
+
+        UserProfile newUserProfile = UserProfileFactory.createUserProfile(userProfile.getUserName(), userProfile.getUserSurname(), userProfile.getGender());
+        userProfileService.create(newUserProfile);
+
+        return new ResponseEntity<UserProfile>(userProfile, HttpStatus.CREATED);
     }
-    @GetMapping ("/read/{userId}")
-    public UserProfile read (@PathVariable String userId){
-    return userProfileService.read(userId);
+
+    @GetMapping("/id/{userId}")
+    public ResponseEntity<?> readByUserProfileId(@PathVariable String userId) {
+        UserProfile userProfile = userProfileService.read(userId);
+        return  new ResponseEntity<UserProfile>(userProfile, HttpStatus.OK);
     }
-    @PostMapping ("/update")
-    public UserProfile update (@RequestBody UserProfile userProfile){
+
+    @GetMapping("/name/{userProfileName}")
+    public ResponseEntity<?> readByUserProfileName(@PathVariable String userName){
+        UserProfile userProfile = userProfileService.readByUserProfileName(userName);
+
+        return new ResponseEntity<UserProfile>(userProfile, HttpStatus.OK);
+    }
+
+    @PostMapping ("/update/")
+    public UserProfile update (@Valid @RequestBody UserProfile userProfile){
     return userProfileService.update(userProfile);
     }
 
--- src/main/java/com/digital/epharmacy/service/User/impl/UserProfileServiceImpl.java
+++ src/main/java/com/digital/epharmacy/service/User/impl/UserProfileServiceImpl.java
@@ -3,52 +3,69 @@ package com.digital.epharmacy.service.User.impl;
 * Author: Nicole Hawthorne
 * Date: 02/09/2020
 * Desc: Implementation for all userprofile methods
+*
+* Author: Nicole Hawthorne
+* Date: 27/10/2020
+* Desc - Auto wired the userProfile Repository. Marked Service class as a service for JPA to read and changed the
+* to update, delete, create that uses JPA functions.
 * */
+import com.digital.epharmacy.controller.ExceptionHandler.MyCustomExceptionHandler;
+import com.digital.epharmacy.entity.Pharmacy.Pharmacy;
 import com.digital.epharmacy.entity.User.UserProfile;
 import com.digital.epharmacy.repository.User.UserProfileRepository;
-import com.digital.epharmacy.repository.User.impl.UserProfileRepositoryImpl;
 import com.digital.epharmacy.service.User.UserProfileService;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class UserProfileServiceImpl implements UserProfileService {
 
-    private static UserProfileService service = null;
+
+    @Autowired
     private UserProfileRepository repository;
 
-    private UserProfileServiceImpl() {
-    this.repository = UserProfileRepositoryImpl.getRepository();
-    }
-
-    public static UserProfileService getService(){
-        if(service == null)
-           service = new UserProfileServiceImpl();
-        return  service;
-    }
 
     @Override
     public Set<UserProfile> getAll() {
-        return this.repository.getAll();
+        return this.repository.findAll().stream().collect(Collectors.toSet());
     }
 
     @Override
     public UserProfile create(UserProfile userProfile) {
-        return this.repository.create(userProfile);
+        return this.repository.save(userProfile);
     }
 
     @Override
     public UserProfile read(String s) {
-        return this.repository.read(s);
+        return this.repository.findById(s).orElseGet(null);
+    }
+
+    @Override
+    public UserProfile readByUserProfileName(String userName) {
+        UserProfile newUserProfile = repository.readByUserProfileName(userName);
+
+        if (newUserProfile == null)
+            throw new MyCustomExceptionHandler("Username name or id does not exist");
+
+        return newUserProfile;
     }
 
     @Override
     public UserProfile update(UserProfile userProfile) {
-        return this.repository.update(userProfile);
+        if (this.repository.existsById(userProfile.getUserId())){
+        return this.repository.save(userProfile);}
+
+        return null;
     }
 
     @Override
     public boolean delete(String s) {
-        return this.repository.delete(s);
+         this.repository.deleteById(s);
+         if (this.repository.existsById(s)) return false;
+         else return  true;
     }
 }
