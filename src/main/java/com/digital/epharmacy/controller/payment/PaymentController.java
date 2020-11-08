b87700a445fb14f2532f07fa99497faacb7ba843
MatthewPearce216 <pearcematthew99@gmail.com>
2020-11-08 22:44:43 +0200

    ContactInformation and Payment Security

--- src/main/java/com/digital/epharmacy/controller/payment/PaymentController.java
+++ src/main/java/com/digital/epharmacy/controller/payment/PaymentController.java
@@ -3,10 +3,16 @@ package com.digital.epharmacy.controller.payment;
 
 import com.digital.epharmacy.entity.Payment.Payment;
 import com.digital.epharmacy.factory.Payment.PaymentFactory;
+import com.digital.epharmacy.service.Payment.PaymentService;
 import com.digital.epharmacy.service.Payment.impl.PaymentServiceImpl;
+import com.digital.epharmacy.service.Validation.ValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Set;
 
 @RestController
@@ -16,19 +22,40 @@ public class PaymentController {
     @Autowired
     private PaymentServiceImpl paymentService;
 
+    @Autowired
+    private ValidationService validationService;
+
     @PostMapping("/create")
-    public Payment create(@RequestBody Payment payment) {
-        Payment newPayment = PaymentFactory.makePayment(payment.getUserID(), payment.getOrderNumber(), payment.getPharmacyID(), payment.getTypeOfPayment(), payment.getPaymentTotal());
-        return paymentService.create(newPayment);
+    public ResponseEntity<Payment> create(@Valid @RequestBody Payment payment, BindingResult result) {
+        ResponseEntity<Payment>  errorMap = (ResponseEntity<Payment>) validationService.MapValidationService(result);
+
+        if(errorMap != null){
+
+            return errorMap;
+        }
+
+        Payment newPayment = PaymentFactory
+                .makePayment(
+                        payment.getUserID(),
+                        payment.getOrderNumber(),
+                        payment.getPharmacyID(),
+                        payment.getTypeOfPayment(),
+                        payment.getPaymentTotal()
+                );
+        paymentService.create(newPayment);
+
+        return new ResponseEntity<Payment>(newPayment, HttpStatus.CREATED);
     }
 
     @GetMapping("/read/{userId}")
-    public Payment read(@PathVariable String userId){
-        return paymentService.read(userId);
+    public ResponseEntity<Payment> read (@PathVariable String userID){
+
+        Payment newPayment = paymentService.read(userID);
+        return new ResponseEntity<Payment>(newPayment, HttpStatus.OK);
     }
 
     @PostMapping("/update")
-    public Payment update(@RequestBody Payment payment){
+    public Payment update(@Valid @RequestBody Payment payment){
         return paymentService.update(payment);
     }
 
