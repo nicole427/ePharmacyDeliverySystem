e8eb2097474bbbaba43569214830eee608904f54
nelson <nelsonmpyana@gmail.com>
2020-11-08 22:13:54 +0200

    resolved conflicts

--- src/main/java/com/digital/epharmacy/security/SecurityConfig.java
+++ src/main/java/com/digital/epharmacy/security/SecurityConfig.java
@@ -31,9 +31,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
               .withUser("UserProfile")
               .password(encoder().encode("54321"))
               .roles(User_Role)
-              .and().withUser("CatalogueItemUser").password(encoder().encode("catlogueitemuserpassword")).roles(User_Role)
-              .and().withUser("PharmacyCalatalogueUser").password(encoder().encode("pharmacycataloguepassword")).roles(User_Role)
-              .and().withUser("PrescriptionItemUser").passsword(encoder().encode("prescriptitempassword")).roles(User_Role)
+              .and()
+              .withUser("CatalogueItemUser")
+              .password(encoder().encode("catlogueitemuserpassword"))
+              .roles(User_Role)
+              .and()
+              .withUser("PharmacyCalatalogueUser")
+              .password(encoder().encode("pharmacycataloguepassword"))
+              .roles(User_Role)
+              .and()
+              .withUser("PrescriptionItemUser")
+              .passsword(encoder().encode("prescriptitempassword"))
+              .roles(User_Role)
 
       ;
 
